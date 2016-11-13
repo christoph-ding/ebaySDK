898892a78fc3a3722b704b1302ab3af75af91442
Abe <myriasofo@gmail.com>
2016-11-13 16:22:46 -0500

    Install express and confirm server is running

--- server/server.js
+++ server/server.js
@@ -1,2 +1,20 @@
 
+// 'port' to listen to (eg. 8000 / 8080)
+// top should be dependencies > constants
 
+// RUN "node server/server.js"
+// Bad practice to edit package.json directly
+
+// npm gets us express
+// RUN "npm install express --save"
+// want to containerize each "pond" (ie. app) for dependencies
+// Good practice: minimize global libraries and containerize dependency
+
+var express = require('express');
+
+var app = express();
+const PORT = 8000 || production.env.NODE_ENV // This is weird (heroku will interpet)
+
+app.listen(PORT, function () {
+    console.log("Hi I'm running on port:", PORT);
+});
