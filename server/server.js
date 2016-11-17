1d05cea8df9cbc22e71370b5b2271bc637e6d80a
Abe <myriasofo@gmail.com>
2016-11-17 16:03:08 -0500

    Refactor server code

--- server/server.js
+++ server/server.js
@@ -1,96 +1,79 @@
 
-// 'port' to listen to (eg. 8000 / 8080)
-// top should be dependencies > constants
-
-// RUN "node server/server.js"
-// Bad practice to edit package.json directly
-
-// npm gets us express
-// RUN "npm install express --save"
-// https://expressjs.com/en/starter/installing.html
-// want to containerize each "pond" (ie. app) for dependencies
-// Good practice: minimize global libraries and containerize dependency
-
-// server should handle "get" request
-// open up http://localhost:8000 OR "curl http://localhost:8000"
-// alternative is "postman" (api testing)
-
-// instead of 'node server/server.js' using 'nodemon server/server.js'
-
-
-// define "endpoint": place going to relative to root (eg. "/" or "/abe")
-// essentially, server is a dashboard, then endpoints are buttons (same as interface?)
-
-
-// node is NOT opinionated, there are too many ways to do things
-
-
-// git ignore for "node_modules" bc users should not be installing our versions
-
-
-// node signature pattern 1 (api) req/res/cb
-// pattern 2 (i/o) error/data
-
-
-// what to do
-// 1. send html
-
-
+/* DEPENDENCIES */
 var express = require('express');
 var fs = require('fs');
 var path = require('path'); //has useful 'join' shit
 var rootDir = "C:/test/ebaySDK";
 
+/* GLOBAL VARS */
 var app = express();
 const PORT = 8000 || production.env.NODE_ENV // This is weird (heroku will interpet)
+
 
 app.listen(PORT, function () {
     console.log("Hi I'm running on port:", PORT);
 });
 
+/* EXAMPLE: How 'get' works with diff paths (eg. url/ vs url/test vs url/test/test2
+    app.get("/", function (req, res) {
+        res.send('hit root'); // completes the request-response cycle
+    });
+
+    app.get("/test", function () {
+        res.send('hit /test'); // completes the request-response cycle
+    });
+
+    app.get("/test/test2", function () {
+        res.send('hit /test/test2'); // completes the request-response cycle
+    });
+*/
+
+/* EXAMPLE: Edit status
+    app.get("/", function (req, res) {
+        res.status(500).send('sending status of 500');
+    });
+*/
+
+/* EXAMPLE: Serve html to 'get'
+    app.get("/", function (req, res) {
+        var pathToHtml = path.join(rootDir, 'client/index.html');
+
+        fs.readFile(pathToHtml, 'utf8', function (err, data) {
+            if (err)
+                return console.log('error:', err);
+            res.send(data); // completes the request-response cycle
+        });
+
+
+        // Same as fs.readFile, but shorter
+        //res.sendFile(pathToHtml);
+    });
+*/
+/* EXAMPLE: Serve css for html
+    // NOTE: html wants css and href is looking at root folder! wow
+    app.get("/style.css", function (req, res) {
+        var pathToCss = path.join(rootDir, 'client/style.css');
+        res.sendFile(pathToCss);
+    });
+*/
+
+
+
+/* EXAMPLE: Using helper functions to encapsulate reading file vs sending res */
 app.get("/", function (req, res) {
     var pathToHtml = path.join(rootDir, 'client/index.html');
-
-    //res.sendFile(pathToHtml);
-
-    //fs.readFile(pathToHtml, 'utf8', function (err, data) {
-    //    if (err) {
-    //        return console.log('error:', err);
-    //    }
-    //    //console.log('contents:', data);
-    //    res.send(data); // completes the request-response cycle
-    //});
-
-    //res.status(500).send();
-    //res.send('sent msg'); // completes the request-response cycle
+    readFile(pathToHtml);
 });
-
-function endResReqCycle(req, res) {
-    res.send();
-}
 
 function readFile(req, res, cb) {
     fs.readFile(path, 'utf8', function (err, data) {
         if (err)
             return console.log('error:', err);
-        res.send(data); // completes the request-response cycle
+        endResReqCycle(res, data);
     });
 }
 
-// NOTE: href is looking at root folder! wow
-app.get("/style.css", function (req, res) {
-    var pathToCss = path.join(rootDir, 'client/style.css');
-    console.log('style.css', pathToCss);
-    res.sendFile(pathToCss);
-});
+function endResReqCycle(res, data) {
+    res.send(data);
+}
 
-//app.get("/abe", function () {
-//    console.log("You just did GET for 'abe'");
-//});
-
-//function doesEndpointExist(str) {
-//    // check str
-//}
-
-
-// TODO: use express.static to do all of above easily
